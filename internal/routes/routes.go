1b7c4b7bfd2caf7f5c2cc80a23f357556dbd362a
Namith <95267206+Namith667@users.noreply.github.com>
2025-01-27 21:33:45 +0530

    RBAC- protected routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Namith667/GoQuick/internal/db"
 	"github.com/Namith667/GoQuick/internal/handlers"
+	"github.com/Namith667/GoQuick/internal/middleware"
 	"github.com/Namith667/GoQuick/internal/services"
 	"github.com/gorilla/mux"
 )
@@ -21,17 +22,23 @@ func InitRoutes(database db.Database) *mux.Router {
 
 	r.HandleFunc("/health", handlers.HealthCheck).Methods("GET")
 
-	//producr routes
+	//public routes
 	r.HandleFunc("/products", productHandler.GetAllProducts).Methods("GET")
 	r.HandleFunc("/products/{id}", productHandler.GetProductById).Methods("GET")
-	r.HandleFunc("/products", productHandler.CreateProduct).Methods("POST")
-	r.HandleFunc("/products/{id}", productHandler.UpdateProduct).Methods("PUT")
-	r.HandleFunc("/products/{id}", productHandler.DeleteProduct).Methods("DELETE")
+
+	//private routes
+	protected := r.PathPrefix("/admin").Subrouter()
+	protected.Use(middleware.JWTAuthMiddleware)
+	protected.Use(middleware.RequireRole("admin"))
+
+	protected.HandleFunc("/products", productHandler.CreateProduct).Methods("POST")
+	protected.HandleFunc("/products/{id}", productHandler.UpdateProduct).Methods("PUT")
+	protected.HandleFunc("/products/{id}", productHandler.DeleteProduct).Methods("DELETE")
 
 	//auth service
-
 	authService := services.NewAuthService(conn)
 	authHandler := handlers.NewAuthHandler(authService)
+
 	//auth routes
 	r.HandleFunc("/register", authHandler.RegisterUser).Methods("POST")
 	r.HandleFunc("/login", authHandler.Login).Methods("POST")
